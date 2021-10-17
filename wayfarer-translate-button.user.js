508b744307308b6fc02d3cca68ba39abcbd8a385
Haukka3 <haukka33@icloud.com>
2021-10-17 14:03:31 +0300

    Dark div

--- wayfarer-translate-button.user.js
+++ wayfarer-translate-button.user.js
@@ -74,8 +74,8 @@
             var i, a = [];
             if (n.nodeType !== 3) {
                 if (n.childNodes)
-                     for (i = 0; i < n.childNodes.length; ++i)
-                         a = a.concat(recursor(n.childNodes[i]));
+                    for (i = 0; i < n.childNodes.length; ++i)
+                        a = a.concat(recursor(n.childNodes[i]));
             } else
                 a.push(n.data);
             return a;
